f6dc09be618c1647d9f5a958157f0afcce0c9a0e
atanana <tanana@email.ru>
2016-07-15 00:19:35 +0300

    shuffle init

--- app/routes/index.js
+++ app/routes/index.js
@@ -29,7 +29,60 @@ export default Ember.Route.extend({
     },
 
     shufflePlayers() {
-      alert('shuffle players!');
+      const tour = this.get('currentTour') + 1;
+      this.set('currentTour', tour);
+      this.store.findAll('player').then(data => {
+        const players = data.toArray();
+        this.store.findAll('group').then(groups => {
+          const initialGroups = groups.filter(group => group.get('tour') === 0).toArray();
+          const groupsCount = Math.max(Math.ceil(players.length / 6), initialGroups.length);
+          const newGroups = [];
+
+          for (let i = 0; i < groupsCount; i++) {
+            newGroups.push(this.store.createRecord('group', {
+              tour: tour
+            }));
+          }
+
+          players.forEach(player => {
+            const group = findAppropriateGroup(player, newGroups);
+
+            if (group) {
+              group.get('players').pushObject(player);
+            } else {
+              alert(`Не удаётся распределить игрока ${player.get('name')}!`);
+            }
+
+            player.save();
+          });
+
+          newGroups.forEach(group => group.save());
+
+          this.controller.set('model', this.model());
+        });
+      });
     }
   }
 });
+
+function findAppropriateGroup(player, groups) {
+  const playerGroups = extractPlayerGroups(player);
+  return groups.find(group => {
+    let appropriate = false;
+    const players = group.get('players').toArray();
+
+    if (players.length < 6) {
+      appropriate = !players.find(anotherPlayer => intersects(playerGroups, extractPlayerGroups(anotherPlayer)));
+    }
+
+    return appropriate;
+  });
+}
+
+function extractPlayerGroups(player) {
+  return player.get('groups').map(group => group.id);
+}
+
+function intersects(array1, array2) {
+  return array1.filter(item => array2.indexOf(item) !== -1).length > 0;
+}
