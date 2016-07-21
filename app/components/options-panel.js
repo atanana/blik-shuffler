b181cd236233a9084c881722ce9b61fca1d65d88
atanana <tanana@email.ru>
2016-07-21 23:31:12 +0300

    options tour count

--- app/components/options-panel.js
+++ app/components/options-panel.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['inline'],
+
   actions: {
     update() {
       this.get('model').save();
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,5 +5,16 @@ export default Ember.Controller.extend({
     return !!this.get('model.groups')
       .map(group => group.get('tour'))
       .find(tour => tour > 0);
+  }),
+
+  tours: Ember.computed('model.options.toursCount', function () {
+    const tours = [];
+    const toursCount = this.get('model.options.toursCount');
+
+    for (let tour = 0; tour <= toursCount; tour++) {
+      tours.push(tour);
+    }
+
+    return tours;
   })
 });
