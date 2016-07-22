35691cce95c731cac8da1cdc43fe9b8fabc8e0dd
atanana <tanana@email.ru>
2016-07-23 00:04:35 +0300

    results refactoring

--- app/routes/scores.js
+++ app/routes/scores.js
@@ -1,30 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  getOptions() {
-    return this.store.findAll('options')
-      .then(optionsEntities => optionsEntities.get('firstObject'));
-  },
-
   model() {
-    return Ember.RSVP.hash({
-      players: this.store.findAll('player')
-        .then(players => this.getOptions()
-          .then(options => Ember.RSVP.all(
-            players.map(player => player.get('groups')
-              .then(groups => {
-                const totalScore = groups.reduce(function (prev, current) {
-                  return prev + parseInt(current.get('score'));
-                }, 0);
+    return this.store.findAll('options')
+      .then(optionsEntities => optionsEntities.get('firstObject'))
+      .then(options => this.store.findAll('player')
+        .then(players => Ember.RSVP.all(
+          players.map(player => player.get('groups')
+            .then(groups => {
+              const totalScore = groups.reduce(function (prev, current) {
+                return prev + parseInt(current.get('score'));
+              }, 0);
 
-                return {
-                  name: player.get('name'),
-                  totalScore: totalScore,
-                  scores: groups.sortBy('tour').map(group => group.get('score')).splice(1, groups.length - options.get('toursCount') - 1)
-                };
-              }))
-          ))),
-      options: this.getOptions()
-    });
+              return {
+                name: player.get('name'),
+                totalScore: totalScore,
+                scores: groups.sortBy('tour').map(group => group.get('score')).splice(1, groups.length - options.get('toursCount') - 1)
+              };
+            }))
+        ).then(players => {
+          return {
+            players: players,
+            options: options
+          };
+        })));
   }
 });
