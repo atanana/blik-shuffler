7e89b75b7dc81bb81d829950f4a0ade70bd0869c
atanana <tanana@email.ru>
2016-07-23 13:11:07 +0300

    results sorting

--- app/routes/scores.js
+++ app/routes/scores.js
@@ -5,24 +5,57 @@ export default Ember.Route.extend({
     return this.store.findAll('options')
       .then(optionsEntities => optionsEntities.get('firstObject'))
       .then(options => this.store.findAll('player')
-        .then(players => Ember.RSVP.all(
-          players.map(player => player.get('groups')
-            .then(groups => {
-              const totalScore = groups.reduce(function (prev, current) {
-                return prev + parseInt(current.get('score'));
-              }, 0);
-
-              return {
-                name: player.get('name'),
-                totalScore: totalScore,
-                scores: groups.sortBy('tour').map(group => group.get('score')).splice(1, groups.length - options.get('toursCount') - 1)
-              };
-            }))
-        ).then(players => {
-          return {
-            players: players,
-            options: options
-          };
-        })));
+        .then(players => getPlayersData(players, options)
+          .then(players => {
+            return {
+              players: sortPlayers(players),
+              options: options
+            };
+          })));
   }
 });
+
+function sortPlayers(players) {
+  players.sort(function (player1, player2) {
+    return player2.totalScore - player1.totalScore;
+  });
+
+  for (let i = 0; i < players.length;) {
+    const currentScore = players[i].totalScore;
+    let next = i + 1;
+    let placeLabel = next;
+
+    while (next < players.length && players[next].totalScore === currentScore) {
+      next++;
+    }
+
+    if (next !== i + 1) {
+      placeLabel += '-' + next;
+    }
+
+    for (let j = i; j < next; j++) {
+      players[j].place = placeLabel;
+    }
+
+    i = next;
+  }
+
+  return players;
+}
+
+function getPlayersData(players, options) {
+  return Ember.RSVP.all(
+    players.map(player => player.get('groups')
+      .then(groups => {
+        const totalScore = groups.reduce(function (prev, current) {
+          return prev + parseInt(current.get('score'));
+        }, 0);
+
+        return {
+          name: player.get('name'),
+          totalScore: totalScore,
+          scores: groups.sortBy('tour').map(group => group.get('score')).splice(1, groups.length - options.get('toursCount') - 1)
+        };
+      }))
+  );
+}
