7c734878c04df0b2ace120df5d1134b68f5fdfdf
atanana <tanana@email.ru>
2016-07-26 08:34:50 +0300

    settings dialog init

--- app/components/options-panel.js
+++ app/components/options-panel.js
@@ -3,9 +3,22 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['inline'],
 
+  openSettings: false,
+  test: true,
+
   actions: {
-    update() {
+    openSettings() {
+      this.set('openSettings', true);
+    },
+
+    saveOptions() {
       this.get('model').save();
+      this.set('openSettings', false);
+    },
+
+    onCloseInput() {
+      this.set('openSettings', false);
+      this.get('model').rollbackAttributes();
     }
   }
 });
--- app/models/options.js
+++ app/models/options.js
@@ -1,7 +1,10 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import Ember from 'ember';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-  toursCount: attr('number', {defaultValue: 0})
+  toursCount: attr('number', {defaultValue: 0}),
+  allGroups: attr('boolean', {defaultValue: false}),
+  notAllGroups: Ember.computed.not('allGroups')
 });
