399e1a606858d3c8bbe6d4e0f446424463284cca
atanana <tanana@email.ru>
2016-07-28 21:59:03 +0300

    save data

--- app/components/save-data.js
+++ app/components/save-data.js
@@ -7,6 +7,13 @@ export default Ember.Component.extend({
     saveData() {
       const data = localStorage['blik-shuffler'];
       const blob = new Blob([data], {type : 'application/json'});
+      const filename = (new Date()).toLocaleString("ru", {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timezone: 'UTC'
+      }) + '.json';
+      saveAs(blob, filename); // jshint ignore:line
     }
   }
 });
